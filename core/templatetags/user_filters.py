556bf2fb4a4ddfb88d9de6fe500058977df9b1f8
zenger05 <zenger05@yandex.ru>
2022-10-13 23:19:33 +0300

    [feat] Add posts app [feat] Add group app

---
+++ core/templatetags/user_filters.py
@@ -0,0 +1,10 @@
+# core/templatetags/user_filters.py
+from django import template
+# В template.Library зарегистрированы все встроенные теги и фильтры шаблонов;
+# добавляем к ним и наш фильтр.
+register = template.Library()
+
+
+@register.filter
+def addclass(field, css):
+    return field.as_widget(attrs={'class': css})
