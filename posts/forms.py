446077161b5e26df39bb2d81ee681c75f8ccc555
zenger05 <zenger05@yandex.ru>
2022-10-24 18:17:04 +0300

    [feat] Add posts app [feat] Add group app

--- posts/forms.py
+++ posts/forms.py
@@ -1,4 +1,6 @@
-import forms
-
-
+import django.forms
+from .models import PostCreate
 class CreateForm(forms.ModelForm):
+    class Meta:
+        model = PostCreate
+        fields = ['text', 'group']
